3af2055b00cdb473ed39ed9195daeccb67fce2fb
nidhikaushal <nidhikaushal28@gmail.com>
2019-04-01 14:39:15 +0530

    localenv

--- src/main/java/com/example/demo/Controller/KafkaController.java
+++ src/main/java/com/example/demo/Controller/KafkaController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping(value="/kafka")
 public class KafkaController {
